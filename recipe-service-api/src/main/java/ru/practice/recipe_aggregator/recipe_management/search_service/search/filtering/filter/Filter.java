fb5c5c5e3db99332a9759e3ce794dfb4f54204c3
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2025-10-10 21:07:52 +0300

    chore: рефакторинг

--- recipe-service-api/src/main/java/ru/practice/recipe_aggregator/recipe_management/search_service/search/filtering/filter/Filter.java
+++ recipe-service-api/src/main/java/ru/practice/recipe_aggregator/recipe_management/search_service/search/filtering/filter/Filter.java
@@ -1,7 +1,5 @@
 package ru.practice.recipe_aggregator.recipe_management.search_service.search.filtering.filter;
 
-
-
 import ru.practice.recipe_aggregator.recipe_management.model.dto.container.SearchContainer;
 import ru.practice.recipe_aggregator.recipe_management.model.dto.response.RecipeResponseDto;
 
